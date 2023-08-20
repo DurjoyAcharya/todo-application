126bb4abc24b0c40d8f545ca786e33b6c36af9f9
DurjoyAcharya <da-durjoy@outlook.com>
2023-08-21 03:17:24 +0600

    hilla created

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,6 +1,7 @@
 package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * and some desktop browsers.
  *
  */
+
+@PWA(name = "", shortName = "", offline = true)
 @SpringBootApplication
 @Theme(value = "hilla-todo")
 public class Application implements AppShellConfigurator {
